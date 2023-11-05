0686c630332b629d275eaafd96b2cd2e88ba1288
Joseph Tsui <joetsui1994@gmail.com>
2023-11-05 22:10:14 +0000

    added data files

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,27 +1,21 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("./experiment1.csv")
 
 logistic_fun <- function(t) {
-  
   N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
-  
   return(N)
-  
 }
 
-N0 <- ??? #
+N0 <- 879 #
   
-r <- ??? #
+r <- 0.0100086 #
   
-K <- ??? #
+K <- 59999999983 #
 
-ggplot(aes(???,???), data = growth_data) +
-  
+ggplot(aes(t, N), data = growth_data) +
   geom_function(fun=logistic_fun, colour="red") +
-  
-  geom_point()
-
-  #scale_y_continuous(trans='log10')
+  geom_point() +
+  scale_y_continuous(trans='log10')
 
 
