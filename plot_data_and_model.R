e5a94f944e4a74c9a3dfc26dacdee67a9e7d657d
rithika ravishankar <rithika.ravishankar@yahoo.co.uk>
2023-11-07 17:05:23 +0000

    commit

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,6 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment1.csv")
 
 logistic_fun <- function(t) {
   
@@ -10,13 +10,15 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
-  
-r <- ??? #
-  
-K <- ??? #
 
-ggplot(aes(???,???), data = growth_data) +
+
+N0 <- 986.34 #intercept N0
+  
+r <- 0.0100086 #t from previous AVOVA data 
+  
+K <- 6.000e+10 #intercept K 
+
+ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
