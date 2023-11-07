bc774ce4edd0ee356b3c58a6a71cf6168bbce076
Beatrice McWilliams <beatrice.mcwilliams@hertford.ox.ac.uk>
2023-11-07 14:55:29 +0000

    something

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,6 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment1.csv")
 
 logistic_fun <- function(t) {
   
@@ -10,18 +10,19 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
+N0 <- 995 #
   
-r <- ??? #
+r <- 9.990e-03 #
   
-K <- ??? #
+K <- 6e+10
 
-ggplot(aes(???,???), data = growth_data) +
+ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
-  geom_point()
+  geom_point() +
 
-  #scale_y_continuous(trans='log10')
+  scale_y_continuous(trans='log10')
+
 
 
