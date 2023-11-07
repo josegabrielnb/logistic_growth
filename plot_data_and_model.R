58969763a508cc8182c711ff2b0c76e709e72ebb
Phoebe Reid <phoebmayreid@gmail.com>
2023-11-07 15:28:46 +0000

    finish

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,6 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment1.csv")
 
 logistic_fun <- function(t) {
   
@@ -10,18 +10,40 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
+N0 <- 972 # (e^(6.883) as this was our estimate for the intercept on the model on the fit linear model. as the intercept is the number of individuals at time 0. but we log transformed the data so it would be e^ to 6.8 to go back 
   
-r <- ??? #
+r <-  1.004e-02#dont need to log transform
   
-K <- ??? #
+K <- 6.000e+10 #used the intercept output from the second linear model 
 
-ggplot(aes(???,???), data = growth_data) +
+ggplot(aes(t,N), data = growth_data) +
   
-  geom_function(fun=logistic_fun, colour="red") +
+geom_function(fun=logistic_fun, colour="red") + 
   
   geom_point()
+  
+  scale_y_continuous(trans='log10')  
 
-  #scale_y_continuous(trans='log10')
 
 
+  
+#making it log so it is linear 
+  
+
+  
+  geom_function(fun=logistic_fun, colour="red") + 
+  
+  geom_point() +
+  
+  xlab("t") +
+  
+  ylab("y") +
+    
+  scale_y_continuous(trans='log10') 
+  
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+  
+  git config --global user.email "<YOUR_EMAIL>"
+  
+  
