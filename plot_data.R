4bfb7bb4aedd23c45d39edcabb3bf470f2baf80c
William Harris <willharris711@gmail.com>
2023-11-08 13:25:48 +0000

    Committing amended files

--- plot_data.R
+++ plot_data.R
@@ -1,26 +1,27 @@
 #Script to plot the logistic growth data
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment2.csv")
 
 install.packages("ggplot2")
 library(ggplot2)
 
-ggplot(aes(t,N), data = ???) +
+#Plots the data
+ggplot(aes(t,N), data = growth_data) +
   
   geom_point() +
   
-  xlab("t") +
+  xlab("Time") +
   
-  ylab("y") +
+  ylab("Number of cells") +
   
   theme_bw()
 
-ggplot(aes(t,???), data = growth_data) +
+ggplot(aes(t,N), data = growth_data) +
   
   geom_point() +
   
-  xlab("t") +
+  xlab("time") +
   
-  ylab("y") +
+  ylab("log(Number of cells)") +
   
-  scale_y_continuous(trans='log10')
+  scale_y_continuous(trans='log10')#converts the y scale to a log scale
