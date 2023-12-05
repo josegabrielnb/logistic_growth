c49cc5d0a4a8c2d2d1a441f280f2b0bb1f5cbf13
audickinson <150164051+audickinson@users.noreply.github.com>
2023-12-05 22:51:08 +0100

    Annotate plot_data.R

--- plot_data.R
+++ plot_data.R
@@ -1,19 +1,25 @@
 #Script to plot the logistic growth data
 
-growth_data <- read.csv("experiment2.csv")
+growth_data <- read.csv("experiment2.csv") # Reading in the data from csv 2
 
-install.packages("ggplot2")
+install.packages("ggplot2") # Installing ggplot 2, which we will use for plotting
 library(ggplot2)
 
-ggplot(aes(t,N), data = growth_data) +
+# Visualizing time vs population size
+
+ggplot(aes(t,N), data = growth_data) + # Plotting the growth data with time on the x axis and population size on the y axis 
   
-  geom_point() +
+  geom_point() + # Adding individual data points to the graph
   
-  xlab("t") +
+  xlab("t") + # Adding x label
   
-  ylab("y") +
+  ylab("y") + # Adding x label
   
-  theme_bw()
+  theme_bw() # Setting the theme to black and white, removing gray background
+
+
+# Plotting the same data on a log scale
+# Same steps, but this time I log-transform the data. 
 
 ggplot(aes(t,log(N)), data = growth_data) +
   
@@ -23,6 +29,8 @@ ggplot(aes(t,log(N)), data = growth_data) +
   
   ylab("y") +
   
-  scale_y_continuous(trans='log10')
+  scale_y_continuous(trans='log10') +
+  
+  theme_bw() # Added the same theme to make them match
 
   
