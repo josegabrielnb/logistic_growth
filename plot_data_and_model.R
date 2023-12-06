c84fb66bf1a927eabf0772c457381391e7f41ac1
Shanaé Fuller <worc5800@ox.ac.uk>
2023-12-06 23:07:13 +0000

    Completed data analysis

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,27 +1,32 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment1.csv")
 
 logistic_fun <- function(t) {
   
-  N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
+  N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))  #calculate N using logistic growth model equation
   
   return(N)
   
 }
 
-N0 <- ??? #
+N0 <- 975.55 #initial population size
   
-r <- ??? #
+r <- 0.0104 #growth rate
   
-K <- ??? #
+K <- 60000000000 #carrying capacity
 
-ggplot(aes(???,???), data = growth_data) +
+ggplot(aes(t,N), data = growth_data) +  #create the plot
   
-  geom_function(fun=logistic_fun, colour="red") +
+  geom_function(fun=logistic_fun, colour="red") +  #add a red curve showing expected behaviour of the model based on given parameters
   
   geom_point()
 
   #scale_y_continuous(trans='log10')
+
+#make a list of required packages and save 
+sink(file = "package-versions.txt")
+sessionInfo()
+sink()
 
 
