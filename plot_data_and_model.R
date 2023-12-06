61ebd4c86cea532a5d7d9ab7adf8c4ed03e8ec47
Aminah Memon <aminah.memon@hertford.ox.ac.uk>
2023-12-06 18:15:38 +0000

    updated R scripts

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,27 +1,33 @@
 #Script to plot data and model
+install.packages(ggplot)
+library(ggplot2)
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment1 (2).csv")
 
 logistic_fun <- function(t) {
   
   N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
   
   return(N)
-  
 }
 
-N0 <- ??? #
-  
-r <- ??? #
-  
-K <- ??? #
+N0 <- 1330.74 #
 
-ggplot(aes(???,???), data = growth_data) +
+r <- 0.0095218 #
+
+K <- 5.979e+10 #
+
+ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
   geom_point()
 
-  #scale_y_continuous(trans='log10')
+#scale_y_continuous(trans='log10')
+
+sink(file = "package-versions.txt")
+sessionInfo()
+sink()
+
 
 
