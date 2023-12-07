eb2cbe3367822d937bea5922a225bb895a4d2288
p-number-8737 <150150878+p-number-8737@users.noreply.github.com>
2023-12-07 02:06:28 +0000

    Update plot_data_and_model.R

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,7 +1,8 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment1.csv")
 
+#defining logistic growth function to identify population size (N) at a given time (t)
 logistic_fun <- function(t) {
   
   N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
@@ -10,18 +11,30 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
-  
-r <- ??? #
-  
-K <- ??? #
 
-ggplot(aes(???,???), data = growth_data) +
+#define the parameters of the logistic growth model using previous analysis:
+
+N0 <- 1320 #intercept of plot 1, initial population size
   
-  geom_function(fun=logistic_fun, colour="red") +
+r <- 0.0095218 # gradient of plot (given as t in lm), growth rate
   
-  geom_point()
+K <- 5.979e+10 # intercept of plot 2, carrying capacity
 
-  #scale_y_continuous(trans='log10')
+#plot growth data with x-axis as t, and y-axis as N
+ggplot(aes(t,N), data = growth_data) +
+  
+  geom_function(fun=logistic_fun, colour="red") + #logistic growth curve plotted
+  
+  geom_point() + #actual data points added to plot
 
+  scale_y_continuous(trans='log10') #transforms y-axis to log scale
 
+#plot shows there is good fit of the linear model to the actual data points 
+
+#question 3 script:
+N0 <- 1320
+r <- 0.0095218
+t <- 4980
+
+N_t <- N0 * exp(r * t)
+N_t
