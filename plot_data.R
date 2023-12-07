293ce952637e31b91d30c3e71bcf81977b6bc99f
p-number-8737 <150150878+p-number-8737@users.noreply.github.com>
2023-12-07 02:07:22 +0000

    Update plot_data.R

--- plot_data.R
+++ plot_data.R
@@ -1,21 +1,27 @@
 #Script to plot the logistic growth data
 
-growth_data <- read.csv("???")
+#create a data frame by reading in the data from the experiment 1 csv file
+growth_data <- read.csv("experiment1.csv")
 
+#install and load the packages required to plot logistic data
 install.packages("ggplot2")
 library(ggplot2)
 
-ggplot(aes(t,N), data = ???) +
+#produces a ggplot with t (time) on the x-axis and N (population size) on the y-axis
+ggplot(aes(t,N), data = growth_data) +
   
-  geom_point() +
+  geom_point() + #adds points to represent data on the plot
   
-  xlab("t") +
+  xlab("t") + #sets x-axis labels as t
   
-  ylab("y") +
+  ylab("y") + #sets y-axis labels as y
   
-  theme_bw()
+  theme_bw() #applies the black points and white background theme to the plot 
 
-ggplot(aes(t,???), data = growth_data) +
+#produces a logistic plot y = N, x = t
+
+
+ggplot(aes(t,N), data = growth_data) +
   
   geom_point() +
   
@@ -23,4 +29,7 @@ ggplot(aes(t,???), data = growth_data) +
   
   ylab("y") +
   
-  scale_y_continuous(trans='log10')
+  scale_y_continuous(trans='log10') #transforms the y-axis to logarithmic scale using base 10 logarithm
+
+#produces a semi-log transformed plot on y-axis for logistic growth of population
+#using this transformation is helpful for linearising the exponential growth phase of logistic growth data
